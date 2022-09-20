557d33c97c2f8b5fccce9403077ca85e1563404f
조민혁 <chohyuk0725@gmail.com>
2022-09-20 09:34:49 +0900

    logout flow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import Axios from "axios";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import NavBar from "../components/NavBar";
 import { AuthProvider } from "../context/auth";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api`;
+  Axios.defaults.withCredentials = true;
+
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
   return (
     <AuthProvider>
-      {/* {!authRoute && <NavBar />} */}
-      <div className={"pt-12 bg-gray-200 min-h-screen"}>
+      {!authRoute && <NavBar />}
+      <div className={authRoute ? "" : "pt-12 bg-gray-200 min-h-screen"}>
         <Component {...pageProps} />
       </div>
     </AuthProvider>
